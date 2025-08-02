7f0f64800d90324b0f54772be3b6b41c23c7c8a6
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2025-08-02 13:41:35 +0530

    Update 1846_Maximum_element_after_decreasing_and_rearranging.cpp

---
+++ 1846_Maximum_element_after_decreasing_and_rearranging.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n = arr.size();
+        int maxele = -1;
+        sort(arr.begin(), arr.end());
+        
+        for(int i = 0; i<n; i++){
+            if(i==0){
+                arr[0] = 1;
+            }
+            else if(abs(arr[i] - arr[i-1]) > 1){
+                arr[i] = arr[i-1] + 1;
+                
+            }
+            maxele = max(maxele, arr[i]);
+        }
+        return maxele;
+    }
+};
