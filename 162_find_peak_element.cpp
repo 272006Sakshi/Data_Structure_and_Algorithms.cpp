add591ba081db1fe317f1eb7058fcfa95ee173cb
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2025-08-31 21:40:16 +0530

    Update 162_find_peak_element.cpp

--- 162_find_peak_element.cpp
+++ 162_find_peak_element.cpp
@@ -5,16 +5,17 @@ public:
         int l = 0;
         int r = n-1;
 
-        while(left<right){
-            int mid = (left+right)/2;
+        while(l<r){
+            int mid = (l+r)/2;
 
             if(nums[mid]>nums[mid+1]){
-                right = mid;
+                r = mid;
             }
             else{
-                left = mid+1;
+                l = mid+1;
             }
         }
-        return left;
+        return l;
     }
 };
+
