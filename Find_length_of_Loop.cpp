ff85beeff242419642ea0b4476a0ca8aef213fe7
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2025-09-06 15:57:12 +0530

    Update Find_length_of_Loop.cpp

--- Find_length_of_Loop.cpp
+++ Find_length_of_Loop.cpp
@@ -1,33 +1,36 @@
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        int target = 0;
-        sort(nums.begin(), nums.end());
-        set<vector<int>> s;
-        vector<vector<int>> output;
+/*
+class Node {
+ public:
+    int data;
+    Node *next;
 
-        for(int i = 0; i<n;i++){
-            int j = i+1;
-            int k = n-1;
-            while(j<k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    s.insert({nums[i], nums[j], nums[k]});
-                    j++;
-                    k--;
-                } else if (sum < target) {
-                    j++;
-                } else {
-                    k--;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int lengthOfLoop(Node *head) {
+        // code here
+        Node* slow = head;
+        Node* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast){
+                int loopLength = 1;
+                Node* temp = slow->next;
+                while (temp != slow) {
+                   loopLength++;
+                   temp = temp->next;
                 }
+                return loopLength; // 5 - 3+1;
             }
         }
-        for(auto triplets : s)
-            output.push_back(triplets);
-        return output;
-            
-        
-
+        return 0;
     }
 };
