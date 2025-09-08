8fcf828236e58b197208a8937869e18c33248ec5
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2025-09-08 21:49:15 +0530

    Update 2485_Find_Pivot_Index.cpp

--- 2485_Find_Pivot_Index.cpp
+++ 2485_Find_Pivot_Index.cpp
@@ -1,1 +1,20 @@
-int n = nums.size
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int total  = 0;
+        int leftS = 0;
+        for(int i = 0; i<n; i++){
+            total+=nums[i];
+
+        }
+        for(int j = 0;j<n; j++){
+            int rightS = total - leftS - nums[j];
+            if(leftS == rightS){
+                return j;
+            }
+            leftS+=nums[j];
+        }
+        return -1;
+    }
+};
