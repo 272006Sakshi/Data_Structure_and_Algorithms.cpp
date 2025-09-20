061df633305f19f427c672b056ff33bafa532b50
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2025-09-21 00:05:38 +0530

    Update 747._Largest_Number_At_Least_Twice_of_Others.cpp

--- 747._Largest_Number_At_Least_Twice_of_Others.cpp
+++ 747._Largest_Number_At_Least_Twice_of_Others.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
     int dominantIndex(vector<int>& nums) {
-    //     int n = nums.size();
-    // int maxVal = -1, second = -1;
-    // int maxIdx = -1;
+         //    int n = nums.size();
+        //     int maxVal = -1, second = -1;
+        //     int maxIdx = -1;
 
-    // for (int i = 0; i < n; i++) {
-    //     if (nums[i] > maxVal) {
+    //         for (int i = 0; i < n; i++) {
+    //            if (nums[i] > maxVal) {
     //         // shift current max down to second
-    //         second = maxVal;
-    //         maxVal = nums[i];    maxIdx = i;
-    //        } 
-    //         else if (nums[i] > second) {
-    //            second = nums[i];
-    //         }
-    //     }
+    //               second = maxVal;
+    //               maxVal = nums[i];    maxIdx = i;
+    //            } 
+    //           else if (nums[i] > second) {
+    //             second = nums[i];
+    //           }
+    //        }
 
-    //      return (maxVal >= 2 * second) ? maxIdx : -1;
+    //        return (maxVal >= 2 * second) ? maxIdx : -1;
+        // using sorting
+          int n = nums.size();
+    
+          vector<pair<int, int>> pairs;
+          for (int i = 0; i < n; i++) {
+            pairs.push_back({nums[i], i});
+          }
 
-           int n = nums.size();
-    // store (value, index) pairs
-    vector<pair<int, int>> pairs;
-    for (int i = 0; i < n; i++) {
-        pairs.push_back({nums[i], i});
-    }
+         // sort by value
+         sort(pairs.begin(), pairs.end());
 
-    // sort by value
-    sort(pairs.begin(), pairs.end());
+         int largest = pairs[n-1].first;
+         int secondLargest = pairs[n-2].first;
+         int indexLargest = pairs[n-1].second;
 
-    int largest = pairs[n-1].first;
-    int secondLargest = pairs[n-2].first;
-    int indexLargest = pairs[n-1].second;
-
-    return (largest >= 2 * secondLargest) ? indexLargest : -1;
-    }
+         return (largest >= 2 * secondLargest) ? indexLargest : -1;
+        }
 };
+
