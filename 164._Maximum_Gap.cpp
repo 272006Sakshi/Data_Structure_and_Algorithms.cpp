e240fe7fddab86fa35752a18b12a2a16561b03ce
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2025-10-09 20:37:11 +0530

    Update 164._Maximum_Gap.cpp

---
+++ 164._Maximum_Gap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if(nums.size()<1){
+            return 0;
+        }
+        sort(nums.begin(), nums.end());
+        int diff = 0;
+        int maxdiff = 0;
+        for(int i=1; i<nums.size(); i++){
+            diff = nums[i] - nums[i-1];
+            maxdiff = max(maxdiff, diff); 
+        }
+        return maxdiff;
+    }
+};
