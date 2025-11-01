218ab56b772842befbcf32fd0dc48615ae230aa6
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2025-11-01 22:35:37 +0530

    Update 20_Valid_Parentheses.cpp

--- 20_Valid_Parentheses.cpp
+++ 20_Valid_Parentheses.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stc;
+        stack<char> dabba;
         for (char it : s) {
             if (it == '(' || it == '{' || it == '[') {
-                stc.push(it);
+                dabba.push(it);
             } else {
-                if (stc.empty()) return false;
-                char ch = stc.top();
-                stc.pop();
+                if (dabba.empty()) return false;
+                char ch = dabba.top();
+                dabba.pop();
                 if (!((it == ')' && ch == '(') ||
                       (it == ']' && ch == '[') ||
                       (it == '}' && ch == '{'))) {
@@ -16,6 +16,6 @@ public:
                 }
             }
         }
-        return stc.empty();
+        return dabba.empty();
     }
 };
