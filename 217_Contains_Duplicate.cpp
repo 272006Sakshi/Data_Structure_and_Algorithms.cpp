6a8dbee7bdd00a2f8d92cdb13eecfbdcbf99ef76
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2025-11-22 12:02:58 +0530

    Update 217_Contains_Duplicate.cpp

--- 217_Contains_Duplicate.cpp
+++ 217_Contains_Duplicate.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        for(int i = 1; i<n; i++){
-            if(nums[i] == nums[i - 1]){
-                return true;
-            }
+        // Approach - 1
+        // sort(nums.begin(), nums.end());
+        // int n = nums.size();
+        // for(int i = 1; i<n; i++){
+        //     if(nums[i] == nums[i - 1]){
+        //         return true;
+        //     }
         
+        // }
+        // return false;
+
+       // Approach -2  // using map
+        // unordered_map<int, int> mp;
+        // for(int i = 0; i<n; i++){
+        //     mp[nums[i]]++;
+        //     if(mp[nums[i]]>1) return true;
+        // }
+
+        //Approach - 3 // using set
+        unordered_set<int> st;
+        for(int x : nums){
+            if(st.count(x)) return true; 
+            st.insert(x);
         }
+        
         return false;
     }
 };
+
