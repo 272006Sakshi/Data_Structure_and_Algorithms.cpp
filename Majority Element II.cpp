15c5387ebf2b6f38a7aca26190b74dd6a4b71bb8
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2025-12-01 12:01:03 +0530

    Update Majority Element II.cpp

--- Majority Element II.cpp
+++ Majority Element II.cpp
@@ -1,18 +1,22 @@
 class Solution {
-  public:
-    vector<int> findMajority(vector<int>& arr) {
-        // Code here
-        int n = arr.size();
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+      // T.C: O(n)   S.C: O(1)
+        int n = nums.size();
         vector<int> res;
-        unordered_map<int, int> count;
-        
-        for(int i  = 0; i<n; i++){
-            count[arr[i]]++;
+        unordered_map<int, int> mp;
+        for(int i = 0; i<n; i++){
+            mp[nums[i]]++;
         }
-        for(auto &num : count){
-            if(num.second > n/3) res.push_back(num.first);
+        for(auto &it:mp){
+            if(it.second>n/3) res.push_back(it.first);
         }
-        sort(res.begin(), res.end());
         return res;
+
+
+      // Optimal Approach
+      // T.C: O(n)  S.C: O(1)
+      
     }
 };
+
