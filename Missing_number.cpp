7e90a662eaf32475736a9d18a329823741878152
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2025-12-18 13:24:35 +0530

    Update Missing_number.cpp

--- Missing_number.cpp
+++ Missing_number.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int N = nums.size();
-        for(int i = 0; i<nums.size();i++){
-            N += i - nums[i];
-        }
-        return N;
+        int n = nums.size();
+    //Approach1: T.C: O(n), S.C: O(1)
+
+        // int sum = n*(n+1)/2;
+        // int totSum = 0;
+
+        // for(int i = 0; i<n; i++){
+        //    totSum+=nums[i];
+        // }
+        // int res = sum-totSum;
+        // return res;
+
+    //Approach1: T.C: O(n), S.C: O(1)
+        int res = n;
+        for(int i = 0; i<n; i++){
+            res += i-nums[i];
+        } 
+        return res;    
     }
 };
