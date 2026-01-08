f24b9be5ae9083390256005e39aea8f363623da0
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2026-01-08 19:13:55 +0530

    Update 103. Binary Tree Zigzag Level Order Traversal.cpp

--- 103. Binary Tree Zigzag Level Order Traversal.cpp
+++ 103. Binary Tree Zigzag Level Order Traversal.cpp
@@ -25,5 +25,33 @@ public:
         vector<vector<int>> res;
         levelArray(root, res, 0);
         return res;
+        
+        // vector<vector<int>> res;
+        // if (!root) return res;
+
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // bool leftToRight = true;
+
+        // while (!q.empty()) {
+        //     int n = q.size();
+        //     vector<int> lev(n);
+
+        //     for (int i = 0; i < n; i++) {
+        //         TreeNode* node = q.front();
+        //         q.pop();
+
+        //         int index = leftToRight ? i : (n - 1 - i);
+        //         lev[index] = node->val;
+
+        //         if (node->left) q.push(node->left);
+        //         if (node->right) q.push(node->right);
+        //     }
+
+        //     res.push_back(lev);
+        //     leftToRight = !leftToRight; 
+        // }
+
+        // return res;
     }
 };
