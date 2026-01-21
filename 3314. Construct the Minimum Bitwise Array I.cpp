9cead220efa28b18990f5bee496563992d449b40
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2026-01-21 22:19:19 +0530

    Update 3314. Construct the Minimum Bitwise Array I.cpp

--- 3314. Construct the Minimum Bitwise Array I.cpp
+++ 3314. Construct the Minimum Bitwise Array I.cpp
@@ -1,18 +1,43 @@
 class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
-        //T.C: O(n)
-        //S.C: O(n)
+       
         int n = nums.size();
-        vector<int> res(n, -1);
+        // vector<int> res(n, -1);
+        // for(int i = 0; i<n; i++){
+        //     for(int val = 0; val<=nums[i]; val++){
+        //         if((val | (val+1)) == nums[i]){
+        //             res[i] = val;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return res;
+
+        //Optimization 
+        vector<int> res;
         for(int i = 0; i<n; i++){
-            for(int val = 0; val<=nums[i]; val++){
-                if((val | (val+1)) == nums[i]){
-                    res[i] = val;
-                    break;
-                }
+            if(nums[i]==2){
+                res.push_back(-1);
+                continue;
             }
+            bool flag = false;
+            for(int j = 1; j<32; j++){
+                if((nums[i]&(1<<j)) >0){ // bit set 
+                    continue;
+                }
+                int prev_bit = j-1;
+                //set 0
+                int val = (nums[i]^(1<<(j-1)));
+                res.push_back(val);
+                flag = true;
+                break;
+            }
+            if(flag == false) res.push_back(-1);
         }
         return res;
+
+        
+    
     }
 };
