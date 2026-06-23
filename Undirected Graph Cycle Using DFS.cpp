4e5da3a47a4495bacaba87bdd3f0455af1f96b6d
Sakshi Kumari <165243176+272006Sakshi@users.noreply.github.com>
2026-06-23 16:38:52 +0530

    Create Undirected Graph Cycle Using DFS.cpp

---
+++ Undirected Graph Cycle Using DFS.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    bool dfs(int node, int parent, vector<int> adj[], vector<bool> &visited){
+        visited[node] = true;
+        for(int u: adj[node]){
+            if(!visited[u]){
+                if(dfs(u, node, adj, visited)) return true;
+            }
+            else if(u!=parent){
+                return true;
+            }
+        }
+    }
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        // Code here
+        vector<int> adj[V];
+        for(auto &e:edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+        vector<bool> visited(V, false);
+        for(int i=0;i<V;i++){
+            if(!visited[i] && dfs(i, -1, adj, visited)) return true;
+        }
+        return false;
+    }
+};
